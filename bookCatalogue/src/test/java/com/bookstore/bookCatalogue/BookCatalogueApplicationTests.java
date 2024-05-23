ab83d2a3f2644483cc79778c240587dc28549c99
shivtaware <61158015+shivtaware@users.noreply.github.com>
2024-05-23 13:50:12 +0530

    Added Book Catalogue

---
+++ bookCatalogue/src/test/java/com/bookstore/bookCatalogue/BookCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookCatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
